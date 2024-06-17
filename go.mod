761f0eb963a5f9a5d3cb5c2082001d3eff500af8
ethan <xht_xht@qq.com>
2024-06-17 22:01:21 +0800

    add code

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ethanvc/evol.git
+
+go 1.22
+
+require (
+	golang.org/x/sys v0.18.0 // indirect
+	google.golang.org/grpc v1.64.0 // indirect
+)
---
+++ obs/status.go
@@ -0,0 +1,63 @@
+package obs
+
+import (
+	"bytes"
+	"fmt"
+	"google.golang.org/grpc/codes"
+)
+
+type Status struct {
+	code  codes.Code
+	event string
+	msg   string
+}
+
+func New(code codes.Code, event string) *Status {
+	return &Status{
+		code:  code,
+		event: event,
+	}
+}
+
+func (s *Status) GetCode() codes.Code {
+	if s == nil {
+		return codes.OK
+	}
+	return s.code
+}
+
+func (s *Status) GetEvent() string {
+	if s == nil {
+		return ""
+	}
+	return s.event
+}
+
+func (s *Status) GetMsg() string {
+	if s == nil {
+		return ""
+	}
+	return s.msg
+}
+
+func (s *Status) SetMsg(msg string, args ...any) *Status {
+	s.msg = fmt.Sprintf(msg, args...)
+	return s
+}
+
+func (s *Status) Error() string {
+	if s == nil {
+		return codes.OK.String()
+	}
+	var buf bytes.Buffer
+	buf.WriteString(s.code.String())
+	if s.event != "" {
+		buf.WriteByte(';')
+		buf.WriteString(s.event)
+	}
+	if s.msg != "" {
+		buf.WriteByte(';')
+		buf.WriteString(s.msg)
+	}
+	return buf.String()
+}
