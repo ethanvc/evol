bdb00de0247f24ec5237ce97ae1d06d74ef7c8b4
hx <xht_xht@qq.com>
2024-10-22 14:43:55 +0800

    add code

--- httpkit/single_attempt.go
+++ httpkit/single_attempt.go
@@ -1,9 +1,21 @@
 package httpkit
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+)
 
 type SingleAttempt struct {
 	Request  *http.Request
 	Response *http.Response
-	err      error
+	Err      error
+}
+
+func NewSingleAttempt(c context.Context, method, url string) *SingleAttempt {
+	sa := &SingleAttempt{}
+	sa.Request, sa.Err = http.NewRequestWithContext(c, method, url, nil)
+	if sa.Err != nil {
+		sa.Request, _ = http.NewRequestWithContext(c, http.MethodGet, "http://127.0.10.10/url_or_method_error", nil)
+	}
+	return sa
 }
