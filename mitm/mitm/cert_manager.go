fa22ce8f1cffc7f3e6501483268830a6f70ea0fa
ethan <xht_xht@qq.com>
2024-11-12 22:13:57 +0800

    add code

--- mitm/mitm/cert_manager.go
+++ mitm/mitm/cert_manager.go
@@ -8,6 +8,8 @@ import (
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"github.com/ethanvc/evol/base"
+	"google.golang.org/grpc/codes"
+	"log/slog"
 	"math"
 	"math/big"
 	"os"
@@ -21,9 +23,11 @@ type CertManager struct {
 
 func NewCertManager() (*CertManager, error) {
 	mgr := &CertManager{}
-	err := mgr.CreateRootCert()
-	if err != nil {
-		return nil, err
+	if err := mgr.loadRootCert(); err != nil {
+		slog.Error("LoadRootCertFailedAndTryCreate")
+		if err := mgr.CreateRootCert(); err != nil {
+			return nil, err
+		}
 	}
 	return mgr, nil
 }
@@ -63,18 +67,21 @@ func (mgr *CertManager) CreateRootCert() error {
 	}
 	mgr.rootCert = &caTemplate
 	content := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: caCert})
-	err = os.WriteFile("root.crt.pem", content, 0600)
+	err = os.WriteFile(rootCertPath, content, 0600)
 	if err != nil {
 		return base.ErrWithCaller(err)
 	}
 	content = x509.MarshalPKCS1PrivateKey(caKey)
 	content = pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: content})
-	err = os.WriteFile("root.key.pem", content, 0600)
+	err = os.WriteFile(rootKeyPath, content, 0600)
 	if err != nil {
 		return base.ErrWithCaller(err)
 	}
 	return nil
 }
+
+const rootCertPath = "root.crt.pem"
+const rootKeyPath = "root.key.pem"
 
 func (mgr *CertManager) CreateDomainCert(domain string) (*tls.Certificate, error) {
 	caKey, err := rsa.GenerateKey(rand.Reader, 2048)
@@ -107,4 +114,35 @@ func (mgr *CertManager) CreateDomainCert(domain string) (*tls.Certificate, error
 		return nil, err
 	}
 	return &certPair, nil
+}
+
+func (mgr *CertManager) loadRootCert() error {
+	certPEM, err := os.ReadFile(rootCertPath)
+	if err != nil {
+		return base.ErrWithCaller(err)
+	}
+	block, _ := pem.Decode(certPEM)
+	if block == nil || block.Type != "CERTIFICATE" {
+		return base.New(codes.Internal).SetEvent("CertFileContentNotRight")
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return base.ErrWithCaller(err)
+	}
+	mgr.rootCert = cert
+
+	keyPEM, err := os.ReadFile(rootKeyPath)
+	if err != nil {
+		return base.ErrWithCaller(err)
+	}
+	block, _ = pem.Decode(keyPEM)
+	if block == nil || block.Type != "RSA PRIVATE KEY" {
+		return base.New(codes.Internal).SetEvent("PrivateKeyTypeNotRight")
+	}
+	key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return base.ErrWithCaller(err)
+	}
+	mgr.rootKey = key
+	return nil
 }
